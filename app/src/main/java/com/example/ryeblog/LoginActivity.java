f8fb365e1112ee738bf3893c80355c4eb1d45133
Ryan Guillen <ryantguillen@gmail.com>
2023-04-27 10:12:07 -0400

    added basic login functionality

--- app/src/main/java/com/example/ryeblog/LoginActivity.java
+++ app/src/main/java/com/example/ryeblog/LoginActivity.java
@@ -2,8 +2,13 @@ package com.example.ryeblog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,11 +16,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SharedPreferences sharedPref = this.getSharedPreferences("application", Context.MODE_PRIVATE);
+        TextView login = findViewById(R.id.login);
+        login.setText(sharedPref.getString("USERNAME", "no val"));
     }
 
 
 
     public void onLogin(View view) {
-        System.out.println("Login Button Clicked");
+        EditText editText = findViewById(R.id.username);
+        String username = editText.getText().toString();
+        if (!username.equals("")) {
+            SharedPreferences sharedPref = this.getSharedPreferences("application", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("USERNAME", username);
+            editor.apply();
+
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+
     }
 }
