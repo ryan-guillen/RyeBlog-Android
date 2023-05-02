6b8e68a0f37e362cb318c1ce82c069b324f18dd7
Ryan Guillen <ryantguillen@gmail.com>
2023-05-02 18:43:07 -0400

    logging in now checks if account exists

--- app/src/main/java/com/example/ryeblog/LoginActivity.java
+++ app/src/main/java/com/example/ryeblog/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.ryeblog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
@@ -11,7 +12,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ryeblog.DB.User;
+import com.example.ryeblog.DB.UserDatabase;
+import com.example.ryeblog.DB.UserViewModel;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +32,35 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         SharedPreferences sharedPref = this.getSharedPreferences("application", Context.MODE_PRIVATE);
-        //if already logged in, go to MainActivity instead, unless you for here from logout
+        // if already logged in, go to MainActivity instead, unless you for here from logout
         if (!sharedPref.getString("USERNAME", "-1").equals("-1") && !loggedOut) {
             Intent i = new Intent(this, MainActivity.class);
             startActivity(i);
         }
 
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        userViewModel.getAllUsers().getValue();
     }
 
     public void onLogin(View view) {
         EditText editText = findViewById(R.id.username);
         String username = editText.getText().toString();
         if (!username.equals("")) {
-            SharedPreferences sharedPref = this.getSharedPreferences("application", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putString("USERNAME", username);
-            editor.apply();
+            UserDatabase.getUser(username, (User user) -> {
+                if (user == null) {
+                    Toast.makeText(this, "That account does not exist.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    SharedPreferences sharedPref = this.getSharedPreferences("application", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("USERNAME", username);
+                    editor.apply();
 
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i);
+                    Intent i = new Intent(this, MainActivity.class);
+                    startActivity(i);
+                }
+            });
         }
         else {
             Toast.makeText(this, "Please enter in a username", Toast.LENGTH_SHORT).show();
